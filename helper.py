620a980a535f5bddd1caa03dce8b5df2a52e9008
Chris Jacobs <zonk1024@gmail.com>
2013-10-29 04:57:41 -0700

    Log level args work. Just... better

--- helper.py
+++ helper.py
@@ -1,57 +1,67 @@
 #!/usr/bin/env python
 
 import os
-HOME_DIR = os.getenv('HOME')
-PROJECT_DIR = '{}/projects'.format(HOME_DIR)
-LOG_DIR = '{}/log'.format(HOME_DIR)
-
 import sys
+
+
+SETTINGS = {}
+try:
+    from env_settings import SETTINGS
+except ImportError:
+    pass
+HOME_DIR = os.getenv('HOME')
+PROJECT_DIR = SETTINGS.get('project_dir', '{}/projects'.format(HOME_DIR))
+LOG_DIR = SETTINGS.get('log_dir', '{}/log'.format(HOME_DIR))
 if PROJECT_DIR not in sys.path:
     sys.path.append(PROJECT_DIR)
 
 import logging
 import optparse
 
+
+def find_main_file_base():
+    """
+    Returns PYTHON_SHELL if there is no __file__ for __main__
+    """
+    if hasattr(sys.modules['__main__'], '__file__'):
+        return os.path.split(sys.modules['__main__'].__file__)[1].rstrip('.py')
+    return 'PYTHON_SHELL'
+
+def mkdir(dirname):
+    """
+    Works like -p
+    """
+    base_dir, actual_dir = os.path.split(dirname)
+    if actual_dir:
+        mkdir(base_dir)
+    if not os.path.isdir(dirname):
+        os.mkdir(dirname)
+
 class Helper(object):
+    """
+    Helper class which combines the logger and option parser
+    """
+    LOG_FILE = '{}/{}.log'.format(LOG_DIR, find_main_file_base())
+    LOG_LEVEL = logging.INFO
     def __init__(self):
-        try:
-            import env_settings
-            self.env_settings = env_settings.settings
-            del(env_settings)
-        except ImportError:
-            pass
-        self.log_level = logging.INFO
-        self.null_handler = None
-        self.stream_handler = None
-        self.file_handler = None
-        self.stdout = True
-        self.to_file = True
-        self.option_usage = 'usage: %prog [options] args'
-        self.main_file_base = self.find_main_file_base()
-        self.log_file = '{}/{}.log'.format(LOG_DIR, self.main_file_base)
-
-        self.option_parser = optparse.OptionParser(self.option_usage)
-        self.option_logger_group = optparse.OptionGroup(self.option_parser, 'Logging Commands')
-        self.option_logger_group.add_option('-l', '--log_stdout', action='store_false', default='true', help='Disable logging to stdout')
-        self.option_logger_group.add_option('-L', '--log_file', action='callback', callback=self.log_file_callback, dest='log_file', default=None, help='Logs to given file, else if naked:\n{}'.format(self.log_file))
-        self.option_logger_group.add_option('-v', '--verbose', action='count', default=0, help='Verbose [may pass multiple]')
-        self.option_logger_group.add_option('-q', '--quiet', action='store_const', const=-1, help='Disable logging to stdout', dest='verbose')
-        self.option_parser.add_option_group(self.option_logger_group)
-
+        self.build_option_parser()
         self.build_options()
+        self.initialize_logger()
         self.setup_logger()
 
-    def find_main_file_base(self):
-        """
-        Logfile is PYTHON_SHELL if there is no __file__ for __main__
-        """
-        if hasattr(sys.modules['__main__'], '__file__'):
-            return os.path.split(sys.modules['__main__'].__file__)[1].rstrip('.py')
-        return 'PYTHON_SHELL'
-
     # Parser functions
-    def add_option(self):
-        self.build_options()
+    def build_option_parser(self):
+        """
+        Function to initially build parser
+        """
+        self.option_usage = 'usage: %prog [options] args'
+        self.option_parser = optparse.OptionParser(self.option_usage)
+        self.option_logger_group = optparse.OptionGroup(self.option_parser, 'Logging Options', 'Helper\'s builtin logging options')
+        self.option_logger_group.add_option('-l', '--log_stdout', action='store_false', default='true', help='Toggle off logging to stdout')
+        self.option_logger_group.add_option('-L', '--log_file', action='callback', callback=self.log_file_callback, dest='log_file', default=None, help='Logs to given file, none if missing, or {} if naked'.format(self.LOG_FILE))
+        self.option_logger_group.add_option('-v', '--verbose', action='count', default=0, help='Increase logging [may pass multiple]')
+        self.option_logger_group.add_option('-q', '--quiet', action='store_const', const=-1, help='Decrease logging level to none', dest='verbose')
+        self.option_parser.add_option_group(self.option_logger_group)
 
     def build_options(self):
         """
@@ -59,24 +69,19 @@ class Helper(object):
         """
         self.options, self.args = self.option_parser.parse_args()
 
+    def add_option(self, *args, **kwargs):
+        """
+        Wrapper to add options to the parser
+        """
+        self.option_parser.add_option(*args, **kwargs)
+        self.build_options()
+
     def log_file_callback(self, option, opt_string, value, parser, args=[], kwargs={}):
         if value is None:
             setattr(parser.values, option.dest, self.log_file)
             return self.log_file
         setattr(parser.values, option.dest, value)
         return value
-
-    # Util functions
-    @classmethod
-    def mkdir(cls, dirname):
-        """
-        Works like -p
-        """
-        base_dir, actual_dir = os.path.split(dirname)
-        if actual_dir:
-            cls.mkdir(base_dir)
-        if not os.path.isdir(dirname):
-            os.mkdir(dirname)
 
     # Logging functions
     def build_log_string(self):
@@ -88,12 +93,24 @@ class Helper(object):
             output += '[Thread:%(thread)s] '
         output += '[%(levelname)s] '
         output += '%(message)s'
-        #print 'build_log_string returning {}'.format(output)
         return output
+
+    def log_level_from_options(self):
+        if self.options.verbose is 0:
+            return self.LOG_LEVEL
+        return min(60, max(0, 50 - (self.options.verbose * 10)))
+
+    def initialize_logger(self):
+        self.null_handler = None
+        self.stream_handler = None
+        self.file_handler = None
+        self.log_level = self.log_level_from_options()
+        self.log_file = self.LOG_FILE
 
     def setup_logger(self):
         self.log_formatter = logging.Formatter(self.build_log_string())
-        self.logger = logging.getLogger(self.main_file_base)
+        self.logger = logging.getLogger(find_main_file_base())
+        self.log_level = self.log_level_from_options()
         self.logger.setLevel(self.log_level)
 
         if self.null_handler is None:
@@ -109,7 +126,7 @@ class Helper(object):
             self.stream_handler.setFormatter(self.log_formatter)
 
         if self.options.log_file and self.file_handler is None:
-            self.mkdir(LOG_DIR)
+            mkdir(LOG_DIR)
             self.file_handler = logging.FileHandler(self.log_file)
             self.file_handler.setLevel(self.log_level)
             self.file_handler.setFormatter(self.log_formatter)
@@ -136,24 +153,32 @@ class Helper(object):
         self.logger.exception(msg)
 
 if __name__ == '__main__':
-    helper = Helper()
-    helper.info('This is a info.')
+    my_helper = Helper()
+    my_helper.info('This is an info.')
     import threading
-    helper.setup_logger()
-    helper.info('This is a info.')
+    my_helper.setup_logger()
+    my_helper.info('This is an info.')
     import multiprocessing
-    helper.setup_logger()
-    helper.info('This is a info.')
+    my_helper.setup_logger()
+    my_helper.info('This is an info.')
     del(threading)
-    helper.setup_logger()
-    helper.info('This is a info.')
+    my_helper.setup_logger()
+    my_helper.info('This is an info.')
     del(multiprocessing)
-    helper.setup_logger()
-    helper.info('This is a info.')
-    print helper.env_settings
-    #helper.debug('This is a debug.')
-    #helper.info('This is a info.')
-    #helper.warning('This is a warning.')
-    #helper.error('This is a error.')
-    #helper.exception('This is a exception.')
-    #helper.critical('This is a critical.')
+    my_helper.setup_logger()
+    my_helper.info('This is an info.')
+    my_helper.info('Settings: {}'.format(SETTINGS))
+    my_helper.info('Options: {}'.format(my_helper.options))
+    my_helper.debug('This is a debug.')
+    my_helper.info('This is an info.')
+    my_helper.warning('This is a warning.')
+    my_helper.error('This is an error.')
+    try:
+        no_way = 1/0
+    except Exception:
+        my_helper.exception('This is an exception.')
+    try:
+        no_way = 1/0
+    except Exception:
+        my_helper.critical('This is a critical.')
+    my_helper.info('This is an info.')
--- helper_tester.py
+++ helper_tester.py
@@ -1,7 +1,33 @@
 #!/usr/bin/env python
 
-from helper import Helper
 
-helper = Helper()
-
-helper.info('Oh hai!')
+if __name__ == '__main__':
+    import helper
+    my_helper = helper.Helper()
+    my_helper.info('This is an info.')
+    import threading
+    my_helper.setup_logger()
+    my_helper.info('This is an info.')
+    import multiprocessing
+    my_helper.setup_logger()
+    my_helper.info('This is an info.')
+    del(threading)
+    my_helper.setup_logger()
+    my_helper.info('This is an info.')
+    del(multiprocessing)
+    my_helper.setup_logger()
+    my_helper.info('This is an info.')
+    my_helper.info(helper.SETTINGS)
+    my_helper.debug('This is a debug.')
+    my_helper.info('This is an info.')
+    my_helper.warning('This is a warning.')
+    my_helper.error('This is an error.')
+    try:
+        no_way = 1/0
+    except Exception:
+        my_helper.exception('This is an exception.')
+    try:
+        no_way = 1/0
+    except Exception:
+        my_helper.critical('This is a critical.')
+    my_helper.info('This is an info.')
